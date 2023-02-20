cf92edef87f373d1f0ae02c068b34d589710a785
Baktyiar Tentimishov <baktyiar.tentimishov@flatexdegiro.com>
2023-02-20 15:36:35 +0100

    app can provide JSON in REST

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [person, setPerson] = useState(null);
+  console.log(person)
+
+  useEffect(() => {
+    fetch('http://localhost:3000/person')
+      .then(response => response.json())
+      .then(person => setPerson(person))
+      .catch(error => console.error(error));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {person ? (
+        <div>
+      <h1>{person.name}</h1>
+      <p>Surname: {person.surname}</p>
+      <p>Age: {person.age}</p>
+      <p>Occupation: {person.occupation}</p>
+    </div>      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
